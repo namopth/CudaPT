7e1043e00591eeffa72a76e3965bf8b8fc42eede
SeeleTH <namo_95@hotmail.com>
2016-01-26 03:56:31 +0900

    refactor device sided bvh and rt

--- include/cudaRTCommon.h
+++ include/cudaRTCommon.h
@@ -1,4 +1,72 @@
 #ifndef CUDARAYTRACERCOMMON_H
 #define CUDARAYTRACERCOMMON_H
+#include "cudahelper.h"
+#include "raytracer.h"
+#include "mathhelper.h"
 
+#include <tbb/parallel_for.h>
+#include <tbb/blocked_range.h>
+
+#include <curand_kernel.h>
+
+#define BVH_DEPTH_MAX 128
+
+extern texture<float4, 1, cudaReadModeElementType> g_bvhMinMaxBounds;
+extern texture<uint1, 1, cudaReadModeElementType> g_bvhOffsetTriStartN;
+extern texture<float4, 1, cudaReadModeElementType> g_triIntersectionData;
+
+struct CURTTexture
+{
+	cudaTextureObject_t texObj;
+	cudaArray* cuArray;
+	uint width;
+	uint height;
+	__hd__ CURTTexture();
+};
+extern CURTTexture* g_devTextures;
+extern std::vector<CURTTexture> g_cuRTTextures; // to CUFREE on CPU Side
+
+extern RTVertex* g_devVertices;
+extern RTTriangle* g_devTriangles;
+extern RTMaterial* g_devMaterials;
+extern float4* g_devBVHMinMaxBounds;
+extern uint1* g_devBVHOffsetTriStartN;
+extern float4* g_devTriIntersectionData;
+
+extern bool g_bIsCudaInit;
+
+struct CURay
+{
+	float3 orig;
+	float3 dir;
+	__hd__ CURay(float3 _orig, float3 _dir);
+	__hd__ float IntersectAABB(const float3& _min, const float3& _max) const;
+
+	__hd__ float IntersectTri(const float3& _p0, const float3& _e0, const float3& _e1, float& w, float& u, float& v, float epsilon = M_EPSILON, bool cullback = true) const;
+};
+
+struct TracePrimitiveResult
+{
+	float dist;
+	int32 triId;
+	float w;
+	float u;
+	float v;
+};
+
+__device__ bool TracePrimitive(const CURay &ray, TracePrimitiveResult& result, const float maxDist = M_INF, const float rayEpsilon = M_EPSILON, bool cullback = true);
+
+__hd__ float4 V32F4(const NPMathHelper::Vec3& vec3);
+__hd__ float3 V32F3(const NPMathHelper::Vec3& vec3);
+
+__device__ void GetMaterialColors(const RTMaterial* mat, const float2 uv, const CURTTexture* textures, float4 &diff, float3 &ambient, float3 &specular, float3 &emissive);
+
+template<class T, int dim, enum cudaTextureReadMode readMode>
+void BindCudaTexture(texture<T, dim, readMode> *tex, void* data, size_t size, uint32 filterMode = cudaFilterModePoint);
+
+CURTTexture CreateCURTTexture(const RTTexture &cpuTex);
+
+void freeAllBVHCudaMem();
+
+void initAllBVHCudaMem(RTScene* scene);
 #endif
---
+++ src/cudaRTDebug.cu
@@ -0,0 +1,138 @@
+#include "cudaRTCommon.h"
+
+#define BLOCK_SIZE 16
+
+namespace cudaRTDebug
+{
+
+float* g_devResultData = nullptr;
+size_t g_resultDataSize = 0;
+
+struct ShootRayResult
+{
+	float4 light;
+};
+
+__device__ ShootRayResult ptDebug_normalRay(const CURay& ray, RTVertex* vertices, RTTriangle* triangles, RTMaterial* materials, CURTTexture* textures)
+{
+	ShootRayResult rayResult;
+
+	TracePrimitiveResult traceResult;
+	if (TracePrimitive(ray, traceResult))
+	{
+		RTTriangle* tri = &triangles[traceResult.triId];
+		RTMaterial* mat = &materials[tri->matInd];
+		RTVertex* v0 = &vertices[tri->vertInd0];
+		RTVertex* v1 = &vertices[tri->vertInd1];
+		RTVertex* v2 = &vertices[tri->vertInd2];
+		float2 uv0 = make_float2(v0->tex._x, v0->tex._y);
+		float2 uv1 = make_float2(v1->tex._x, v1->tex._y);
+		float2 uv2 = make_float2(v2->tex._x, v2->tex._y);
+		float2 uv = uv0 * traceResult.w + uv1 * traceResult.u + uv2 * traceResult.v;
+		float3 n0 = V32F3(v0->norm);
+		float3 n1 = V32F3(v1->norm);
+		float3 n2 = V32F3(v2->norm);
+		float3 norm = n0 * traceResult.w + n1 * traceResult.u + n2 * traceResult.v;
+
+		float4 diff;
+		float3 ambient;
+		float3 specular;
+		float3 emissive;
+		GetMaterialColors(mat, uv, textures, diff, ambient, specular, emissive);
+
+		float3 w = norm;
+		float3 u = normalize(vecCross((fabs(w.x) > .1 ? make_float3(0, 1, 0) : make_float3(1, 0, 0)), w));
+		float3 v = vecCross(w, u);
+
+		rayResult.light = diff;
+	}
+	else
+	{
+		rayResult.light.x = 1.f;
+		rayResult.light.y = 1.f;
+		rayResult.light.z = 1.f;
+		rayResult.light.w = 1.f;
+	}
+
+	return rayResult;
+}
+
+uint32 WangHash(uint32 a) {
+	a = (a ^ 61) ^ (a >> 16);
+	a = a + (a << 3);
+	a = a ^ (a >> 4);
+	a = a * 0x27d4eb2d;
+	a = a ^ (a >> 15);
+	return a;
+}
+
+__global__ void ptDebug_kernel(float3 camPos, float3 camDir, float3 camUp, float3 camRight, float fov,
+	float width, float height, RTVertex* vertices, RTTriangle* triangles, RTMaterial* materials, CURTTexture* textures
+	, float* result)
+{
+	uint x = blockIdx.x * blockDim.x + threadIdx.x;
+	uint y = blockIdx.y * blockDim.y + threadIdx.y;
+	uint ind = (y * width + x) * 3;
+	int threadId = (blockIdx.x + blockIdx.y * gridDim.x) * (blockDim.x * blockDim.y) + (threadIdx.y * blockDim.x) + threadIdx.x;
+
+
+	float u = (2.f * ((float)x + 0.5f) / width - 1.f) * tan(fov * 0.5f) * width / height;
+	float v = (2.f * ((float)y + 0.5f) / height - 1.f) * tan(fov * 0.5f);
+	float3 dir = normalize(camRight * u + camUp * v + camDir);
+	CURay ray(camPos, dir);
+
+	ShootRayResult rayResult = ptDebug_normalRay(ray, vertices, triangles, materials, textures);
+
+	result[ind] = rayResult.light.x;
+	result[ind + 1] = rayResult.light.y;
+	result[ind + 2] = rayResult.light.z;
+}
+
+void cudaPTDebugClean()
+{
+	freeAllBVHCudaMem();
+	CUFREE(g_devResultData);
+}
+
+bool cudaPTDebugRender(NPMathHelper::Vec3 camPos, NPMathHelper::Vec3 camDir, NPMathHelper::Vec3 camUp, float fov, RTScene* scene
+	, float width, float height, float* result)
+{
+	// Check and allocate everything
+	if (!scene || !scene->GetCompactBVH()->IsValid())
+		return false;
+
+	NPMathHelper::Vec3 camRight = camDir.cross(camUp).normalize();
+	camUp = camRight.cross(camDir).normalize();
+
+	if (!g_bIsCudaInit || scene->GetIsCudaDirty())
+	{
+		initAllBVHCudaMem(scene);
+	}
+
+	if (!g_bIsCudaInit)
+		return false;
+
+	if (!g_devResultData || g_resultDataSize != (sizeof(float) * 3 * width * height))
+	{
+		g_resultDataSize = sizeof(float) * 3 * width * height;
+		CUFREE(g_devResultData);
+		cudaMalloc((void**)&g_devResultData, g_resultDataSize);
+	}
+
+	float3 f3CamPos = V32F3(camPos);
+	float3 f3CamUp = V32F3(camUp);
+	float3 f3CamDir = V32F3(camDir);
+	float3 f3CamRight = V32F3(camRight);
+
+	// Kernel go here
+	dim3 block(BLOCK_SIZE, BLOCK_SIZE, 1);
+	dim3 grid(width / block.x, height / block.y, 1);
+	ptDebug_kernel << < grid, block >> > (f3CamPos, f3CamDir, f3CamUp, f3CamRight, fov, width, height, g_devVertices, g_devTriangles, g_devMaterials, g_devTextures
+		, g_devResultData);
+
+	// Copy result to host
+	cudaMemcpy(result, g_devResultData, g_resultDataSize, cudaMemcpyDeviceToHost);
+	return true;
+}
+
+};
